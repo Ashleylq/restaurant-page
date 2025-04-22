8b8eefb3119ef8a234c3cd67abd98b0338b9c65e
Ashleylq <ironmanashley@gmail.com>
2025-04-22 13:16:32 +0000

    add contact

--- src/about.js
+++ src/about.js
@@ -5,10 +5,8 @@ const renderAboutPage = function(){
     }
     const heading = document.createElement('h1');
     heading.textContent = "Soup 'n smoke";
-    heading.setAttribute("class", "heading");
     const about = document.createElement('p');
     about.textContent = `Right off the beach and full of flavor—that’s Soup ‘n Smoke. We’re your go-to chill spot for hearty soups, smoky BBQ, and fully loaded jacket potatoes. Whether you're sandy from the surf or just soaking in the sea breeze, swing by for good eats and laid-back vibes. No dress code, no rush—just comfort food done right by the coast.`;
-    about.setAttribute("class", "about");
     container.appendChild(heading);
     container.appendChild(about);
 }
--- src/contact.js
+++ src/contact.js
@@ -3,6 +3,18 @@ const renderContact = function(){
     while (container.firstChild){
         container.removeChild(container.firstChild);
     }
+    const addressHeader = document.createElement('h2');
+    addressHeader.textContent = "Address";
+    const address = document.createElement('p');
+    address.textContent = "Fort Beach, Kochi, Kerala"
+    const contactHeader = document.createElement('h2');
+    contactHeader.textContent = "Contact us at"
+    const contact = document.createElement('div');
+    contact.innerHTML = "<p>8838820173</p> <p>5637392031</p> <p>SoupnSmokeKochi@gmail.com</p>";
+    container.appendChild(addressHeader);
+    container.appendChild(address);
+    container.appendChild(contactHeader);
+    container.appendChild(contact);
 }
 
 export {renderContact};
